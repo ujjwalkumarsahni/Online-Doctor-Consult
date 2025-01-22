7fcbe6f73ff0393a70d7a4ef31073de4eff7964f
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2025-01-22 18:29:32 +0530

    Contact page done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -1,8 +1,29 @@
 import React from 'react'
+import { assets } from './../assets/assets';
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+    <div>
+      
+      <div className='text-center text-2xl pt-10 text-gray-600'> 
+          <p>CONTACT <span className='font-medium text-gray-700'>US</span></p>
+      </div>
+
+      <div className='my-10 flex flex-col md:flex-row justify-center gap-10 mb-28 text-sm'>
+
+        <img className='w-full md:max-w-[360px]' src={assets.contact_image} alt="" />
+        <div className='flex flex-col items-start gap-6 justify-center'>
+          <p className='font-semibold text-lg text-gray-600'>Our OFFICE</p>
+          <p className='text-gray-500'>54709 Willms Station <br />Suite 350, Washington, USA</p>
+          <p className='text-gray-500'>Tel: (415) 555‑0132 <br /> Email: greatstackdev@gmail.com</p>
+          <p className='font-semibold text-lg text-gray-600'>Careers at PRESCRIPTO</p>
+          <p className='text-gray-500'>Learn more about our teams and job openings.</p>
+          <button className='border border-black px-7 py-3 text-sm hover:bg-black hover:text-white transition-all duration-500'>Explore Jobs</button>
+        </div>
+
+      </div>
+
+    </div>
   )
 }
 
