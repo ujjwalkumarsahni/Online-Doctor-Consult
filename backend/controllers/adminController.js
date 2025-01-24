588b4cae867d28667f3e664faaf130c36235693f
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2025-01-24 23:26:43 +0530

    authnticate admin

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -2,6 +2,7 @@ import validator from 'validator';
 import bcrypt from 'bcrypt';
 import { v2 as cloudinary } from 'cloudinary';
 import doctorModel from '../models/doctorModel.js';
+import jwt from 'jsonwebtoken'
 
 // API for adding a doctor
 const addDoctor = async (req, res) => {
@@ -66,4 +67,20 @@ const addDoctor = async (req, res) => {
     }
 };
 
-export { addDoctor };
+const loginAdmin = async (req,res) =>{
+    try {
+        const {email,password} = req.body;        
+        if(email === process.env.ADMIN_EMAIL && password === process.env.ADMIN_PASSWORD){
+            const token = jwt.sign(email+password, process.env.JWT_SECRET)
+            res.status(200).json({ success: true, message: "Login successful", token });
+
+        }else{
+            return res.status(400).json({ success: false, message: "Invalid email and password" });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).json({ success: false, message: "Server error, please try again later" });
+    }
+}
+
+export { addDoctor, loginAdmin};
