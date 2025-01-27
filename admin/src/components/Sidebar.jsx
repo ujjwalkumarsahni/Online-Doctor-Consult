de000ee94215763ae4d1d09871299d9251ad95df
Ujjwal Kumar <133194307+ujjwalkumarsahni@users.noreply.github.com>
2025-01-27 15:55:16 +0530

    admin add the doctor

--- admin/src/components/Sidebar.jsx
+++ admin/src/components/Sidebar.jsx
@@ -7,22 +7,22 @@ const Sidebar = () => {
     const {aToken} = useContext(AdminContext)
 
   return (
-    <div>
+    <div className='min-h-screen border-r bg-white'>
         {
-            aToken && <ul>
-                <NavLink to='/admin-dashboard'>
+            aToken && <ul className='text-[#515151] mt-5'>
+                <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-blue-600' : ''}`} to={'/admin-dashboard'}>
                     <img src={assets.home_icon} alt="" />
                     <p>Dashboard</p>
                 </NavLink>
-                <NavLink>
-                    <img src={assets.appointments_icon} alt="" />
+                <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-blue-600' : ''}`} to={'/all-appointments'}>
+                    <img src={assets.appointment_icon} alt="" />
                     <p>Appointments</p>
                 </NavLink>
-                <NavLink>
+                <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-blue-600' : ''}`} to={'/add-doctor'}>
                     <img src={assets.add_icon} alt="" />
                     <p>Add Doctor</p>
                 </NavLink>
-                <NavLink>
+                <NavLink className={({isActive}) => `flex items-center gap-3 py-3.5 px-3 md:px-9 md:min-w-72 cursor-pointer ${isActive ? 'bg-[#F2F3FF] border-r-4 border-blue-600' : ''}`} to={'/doctor-list'}>
                     <img src={assets.people_icon} alt="" />
                     <p>Doctors List</p>
                 </NavLink>
